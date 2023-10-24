a63cce748975e0570c49253174590d1b6c3ea39e
r9jgfja <112956409+DenisShestakov1@users.noreply.github.com>
2023-10-24 23:49:27 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,59 +1,57 @@
 #include <iostream>
-#include <cstring>
-#pragma warning(disable : 4996)
-int main() {
-    char source[] = "Hello, ";
-    char destination[100] = "world";
 
- 
-    strcat(destination, source);
-    std::cout << "strcat: " << destination << std::endl;
-
-
-    char copy[100];
-    strcpy(copy, source);
-    std::cout << "strcpy: " << copy << std::endl;
-
-    
-    char strncat_destination[100] = "Hello, ";
-    char strncat_source[] = "world";
-    strncat(strncat_destination, strncat_source, 3);
-    std::cout << "strncat: " << strncat_destination << std::endl;
-
-  
-    char strncpy_copy[100];
-    char strncpy_source[] = "Hello, world";
-    strncpy(strncpy_copy, strncpy_source, 5);
-    std::cout << "strncpy: " << strncpy_copy << std::endl;
-
-   
-    char str1[] = "apple";
-    char str2[] = "banana";
-    int result = strcmp(str1, str2);
-    std::cout << "strcmp: " << result << std::endl;
-
- 
-    char str3[] = "apple";
-    char str4[] = "appetizer";
-    int n = 3;
-    result = strncmp(str3, str4, n);
-    std::cout << "strncmp: " << result << std::endl;
-
-   
-    char str5[] = "Hello, world!";
-    size_t length = strlen(str5);
-    std::cout << "strlen: " << length << std::endl;
-
- std::string mainString = "Hello, world! This is a test string.";
-    std::string searchString = "world";
-
-    size_t found = mainString.find(searchString);
-
-    if (found != std::string::npos) {
-        std::cout << "Substring found at position: " << found << std::endl;
-    } else {
-        std::cout << "Substring not found." << std::endl;
+int my_strcmp(const char* str1, const char* str2) {
+    while (*str1 == *str2) {
+        if (*str1 == '\0') {
+            return 0; 
+        }
+        str1++;
+        str2++;
     }
+    return *str1 - *str2;
+}
+int my_strncmp(const char* str1, const char* str2, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        if (str1[i] == '\0' || str1[i] != str2[i]) {
+            return str1[i] - str2[i];
+        }
+    }
+    return 0;
+}
+   
+
+int main() {
+    const char* str1 = "apple";
+    const char* str2 = "banana";
+    size_t n = 3;
+
+
+    int result = my_strcmp(str1, str2);
+
+    if (result == 0) {
+        std::cout << "Строки идентичны" << std::endl;
+    }
+    else if (result < 0) {
+        std::cout << "Первая строка меньше второй" << std::endl;
+    }
+    else {
+        std::cout << "Вторая строка меньше первой" << std::endl;
+    }
+
+    int result = my_strncmp(str1, str2, n);
+
+    if (result == 0) {
+        std::cout << "Первые " << n << " символа идентичны" << std::endl;
+    }
+    else if (result < 0) {
+        std::cout << "Первые " << n << " символа первой строки меньше второй" << std::endl;
+    }
+    else {
+        std::cout << "Первые " << n << " символа второй строки меньше первой" << std::endl;
+    }
+
+
 
     return 0;
 }
+
